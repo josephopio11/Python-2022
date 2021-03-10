d821ba014bbe6fe7795a7ace6e26f593e556a0b3
josephopio <josephopio11@gmail.com>
2021-03-10 11:54:42 +0200

    Starting with Lesson 8

---
+++ Lesson 8.py
@@ -0,0 +1,19 @@
+# Finding out the length of a string
+statement_1 = "I come from Kinanira"
+statement_2 = "This means I am a Kinanirarian"
+
+print(len(statement_1))
+# Print for me the length of "statement_2"
+
+# Finding out the position of a letter in a string
+student_1 = 'Zoe'
+student_2 = 'Kerria'
+
+print(student_1.index("o"))
+# Exercise: Find out the position of the letter "i"
+# in the name "Kerria"
+
+
+# Counting the number of times a letter occurs in a string
+letters = "Dear friend, from the day you left, I have never taken a shower"
+print(letters.count("e"))
