b46e7b1eada1d2cc1f3e9728e0074e58c0f45b60
josephopio <josephopio11@gmail.com>
2021-03-10 11:58:03 +0200

    Starting with Lesson 8

--- Lesson 8.py
+++ Lesson 8.py
@@ -2,18 +2,21 @@
 statement_1 = "I come from Kinanira"
 statement_2 = "This means I am a Kinanirarian"
 
-print(len(statement_1))
+print("The length of statement_1 is: ", len(statement_1))
 # Print for me the length of "statement_2"
 
 # Finding out the position of a letter in a string
 student_1 = 'Zoe'
 student_2 = 'Kerria'
 
-print(student_1.index("o"))
+print("The position of letter 'o' is ", student_1.index("o"))
 # Exercise: Find out the position of the letter "i"
 # in the name "Kerria"
 
 
 # Counting the number of times a letter occurs in a string
 letters = "Dear friend, from the day you left, I have never taken a shower"
-print(letters.count("e"))
+print("The number of times letter 'e' occurs is", letters.count("e"))
+
+# Exercise: Print for me the number of times the letter "a"
+# occurs in your string "letters"
