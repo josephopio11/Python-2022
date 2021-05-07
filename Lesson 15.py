e7c3c56aa6ff6ebf16528de3f6f22fc0fb1f77de
josephopio11 <josephopio11@gmail.com>
2021-05-07 14:03:20 +0200

    Adjustment of the README file.

--- Lesson 15.py
+++ Lesson 15.py
@@ -1,23 +1,3 @@
 # Functions
 
-# def say_hello():
-#     print("Hello, world!")
-#
-# say_hello()
-
-def greeting(x):
-    print("Good morning, ", x)
-
-
-greeting("Joseph")
-
-
-def adding_two_numbers(number1, number2):
-    addition = number1 + number2
-    print(addition)
-
-
-adding_two_numbers(67, 37)
-adding_two_numbers(3, 7)
-adding_two_numbers(7, 12)
-adding_two_numbers(90, 900)
+["Derrick", "Armel", ""]
