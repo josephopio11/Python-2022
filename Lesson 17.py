4ebec0ff6999a54900d3a69b533e83dbcc5846cc
Joseph Opio <josephopio11@gmail.com>
2021-08-13 12:18:04 +0200

    Modules

---
+++ Lesson 17.py
@@ -0,0 +1,28 @@
+# Dictionaries
+phonebook = {
+    "John": 68541236,
+    "Jane": 79787675,
+    "June": 22334455
+}
+
+# print(phonebook)
+
+phonebook["Darlin"] = 45678912
+phonebook["Winston"] = 85496532
+phonebook["Davy"] = 41285679
+
+# print(phonebook)
+#
+# del phonebook["John"]
+# print(phonebook)
+#
+# phonebook.pop("June")
+# print(phonebook)
+#
+# if "John" in phonebook:
+#     print("John is in the phonebook")
+# else:
+#     print("John is not in the phonebook")
+#
+for name, number in phonebook.items():
+    print("The number of %s is %d\n" % (name, number))
